b4b89b9876aa8f28070b01c4f24d72daec7ab681
Jefferson Lim <jefferer@gmail.com>
2017-06-22 22:09:44 +0000

    [feature] set up files, RESTful routes, initial semantic styling, and test daa

---
+++ app.js
@@ -0,0 +1,100 @@
+var bodyParser  = require("body-parser"),
+methodOverride  = require("method-override"),
+mongoose        = require("mongoose"),
+express         = require("express"),
+app             = express();
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+
+//RESTFUL ROUTES
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+})
+
+//index route
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log("error!!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    })
+})
+
+//new route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+})
+
+//create route
+app.post("/blogs", function(req, res) {
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            // redirect to index
+            res.redirect("/blogs");
+        }
+    })
+})
+
+//show route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+})
+
+//edit route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+})
+
+//update route
+app.put("/blogs/:id", function(req, res) {
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs/" + req.params.id);
+       }
+   })
+})
+
+//delete route
+app.delete("/blogs/:id", function(req, res) {
+    res.send("here is the delete route");
+})
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("SERVER IS RUNNING");
+})
